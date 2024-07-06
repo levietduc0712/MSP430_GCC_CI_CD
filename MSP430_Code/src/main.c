d120392a95dc69bce3e1443ba55d814c19eb2dc7
levietduc0712 <levietduc0712@gmail.com>
2024-07-06 21:58:39 +0700

    first commit

---
+++ MSP430_Code/src/main.c
@@ -0,0 +1,11 @@
+#include <msp430.h>
+
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
+    P1DIR |= 0x01; // Set P1.0 to output direction
+
+    while (1) {
+        P1OUT ^= 0x01; // Toggle P1.0 using exclusive-OR
+        __delay_cycles(100000);
+    }
+}
