17897fd0a685c4d56929bbe44c70bb01700988d2
levietduc0712 <levietduc0712@gmail.com>
2024-07-23 17:46:58 +0700

    update

--- MSP430_Code/src/main.c
+++ MSP430_Code/src/main.c
@@ -1,10 +1,23 @@
-#include <msp430.h>
+#include <gpio.h>
+#include <clock_init.h>
 
-void blink_fast(void) {
-    P1DIR |= 0x02; // Set P1.1 to output direction
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
 
-    while (1) {
-        P1OUT ^= 0x02; // Toggle P1.1 using exclusive-OR
-        __delay_cycles(50000);
+    clock_init();
+    
+    gpio_init(GPIO_PORT_P1, GPIO_PIN0, GPIO_DIR_OUTPUT, GPIO_OUT_LOW);
+    gpio_init(GPIO_PORT_P9, GPIO_PIN7, GPIO_DIR_OUTPUT, GPIO_OUT_LOW);
+
+    while(1)
+    {
+        gpio_set(GPIO_PORT_P1, GPIO_PIN0, GPIO_OUT_HIGH);
+        gpio_set(GPIO_PORT_P9, GPIO_PIN7, GPIO_OUT_HIGH);
+        __delay_cycles(16000000);
+        gpio_set(GPIO_PORT_P1, GPIO_PIN0, GPIO_OUT_LOW);
+        gpio_set(GPIO_PORT_P9, GPIO_PIN7, GPIO_OUT_LOW);
+        __delay_cycles(16000000);
     }
+
 }
