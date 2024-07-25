95e433421d04d185cd41b7caea9ed78b458ef46c
levietduc0712 <levietduc0712@gmail.com>
2024-07-25 11:46:46 +0700

    update gpio driver and add lcd  driver

--- MSP430_Code/src/main.c
+++ MSP430_Code/src/main.c
@@ -10,21 +10,21 @@ int main(void)
     clock_init();                           // Initialize clock to 16MHz
     timerA_init();
 
-    gpio_init(GPIO_PORT_P1, GPIO_PIN0, GPIO_DIR_OUTPUT, GPIO_OUT_LOW);
-    gpio_init(GPIO_PORT_P9, GPIO_PIN7, GPIO_DIR_OUTPUT, GPIO_OUT_LOW);
-    gpio_init(GPIO_PORT_P9, GPIO_PIN2, GPIO_DIR_OUTPUT, GPIO_OUT_LOW);
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_setAsOutputPin(GPIO_PORT_P9, GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P9, GPIO_PIN2);
 
     unsigned long previousMillis = 0;
 
     while(1)
     {
-        
+
         unsigned long currentMillis = millis();
 
         if ((currentMillis - previousMillis) >= INTERVAL) {
-            gpio_toggle(GPIO_PORT_P1, GPIO_PIN0);
-            gpio_toggle(GPIO_PORT_P9, GPIO_PIN7);
-            gpio_toggle(GPIO_PORT_P9, GPIO_PIN2);
+            GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+            GPIO_toggleOutputOnPin(GPIO_PORT_P9, GPIO_PIN7);
+            GPIO_toggleOutputOnPin(GPIO_PORT_P9, GPIO_PIN2);
             previousMillis = currentMillis;  // Update the last time toggling happened
         }
 
