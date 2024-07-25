4243fce70fb5b622a7062a0887e3627dda269e8a
levietduc0712 <levietduc0712@gmail.com>
2024-07-25 13:32:56 +0700

    update lcd driver

--- MSP430_Code/src/main.c
+++ MSP430_Code/src/main.c
@@ -1,5 +1,6 @@
-#include <gpio.h>
-#include <clock_init.h>
+#include "gpio.h"
+#include "lcd.h"
+#include "clock_init.h"
 
 #define INTERVAL 500
 
@@ -9,12 +10,15 @@ int main(void)
 
     clock_init();                           // Initialize clock to 16MHz
     timerA_init();
+    Init_LCD();
+    clearLCD();
 
     GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
     GPIO_setAsOutputPin(GPIO_PORT_P9, GPIO_PIN7);
     GPIO_setAsOutputPin(GPIO_PORT_P9, GPIO_PIN2);
 
     unsigned long previousMillis = 0;
+    showString("HELLO");
 
     while(1)
     {
